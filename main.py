30804b880ef8c9045575c39ec47b4e51d73f8d53
Maks-Diablo <v16111980@gmail.com>
2024-10-18 00:39:04 +0300

    Separated auntefication from the card creation class

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import multiprocessing
 import json
 import random
 import time
-from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import sys
 from functools import wraps
 import logging
+
+from auth_manager import AuthManager
 
 # Настройка логирования
 logging.basicConfig(
@@ -55,6 +54,7 @@ def retry_on_message(max_retries=5, base_delay=10, message="попробуйте
 
     return decorator
 
+
 def retry_on_exception(max_retries=5, base_delay=10, backoff_factor=2):
     """
     Декоратор для повторной попытки выполнения метода при возникновении любого исключения.
@@ -63,6 +63,7 @@ def retry_on_exception(max_retries=5, base_delay=10, backoff_factor=2):
     :param base_delay: Базовая задержка перед повторной попыткой в секундах.
     :param backoff_factor: Фактор увеличения задержки для экспоненциального роста.
     """
+
     def decorator(func):
         @wraps(func)
         def wrapper(self, wait, *args, **kwargs):
@@ -76,14 +77,18 @@ def retry_on_exception(max_retries=5, base_delay=10, backoff_factor=2):
                 except Exception as e:
                     logging.warning(f"Попытка {attempt} из {max_retries} для шага '{func.__name__}' не удалась: {e}")
                     if attempt == max_retries:
-                        logging.error(f"Достигнуто максимальное количество попыток ({max_retries}) для шага '{func.__name__}'. Пропуск шага.")
+                        logging.error(
+                            f"Достигнуто максимальное количество попыток ({max_retries}) для шага '{func.__name__}'. Пропуск шага.")
                         raise  # Пробрасываем исключение дальше после исчерпания попыток
                     logging.info(f"Ожидание {delay} секунд перед повторной попыткой.")
                     time.sleep(delay)
                     delay *= backoff_factor  # Увеличиваем задержку
                     attempt += 1
+
         return wrapper
+
     return decorator
+
 
 class PlayerokAutomation:
     SECTION_MAPPING = {
@@ -103,27 +108,15 @@ class PlayerokAutomation:
     MAX_RETRIES = 100  # Максимальное количество попыток
     BASE_DELAY = 20  # Базовая задержка в секундах для повторных попыток
 
-    def __init__(self, section_number, card, product_data, virt_description):
+    def __init__(self, section_number, card, product_data, virt_description, auth_manager):
         self.section_number = section_number
         self.section_name = self.SECTION_MAPPING.get(section_number)
         self.full_section_name = self.load_section_names()
         self.card = card
         self.product_data = product_data
         self.virt_description = virt_description
-        self.driver = self.init_driver()
+        self.auth_manager = auth_manager
         self.url = ""
-
-    def init_driver(self):
-        """Инициализация драйвера с опциями для оптимизации."""
-        options = Options()
-        # options.add_argument("--headless")  # Запуск без интерфейса браузера
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("--no-sandbox")
-        # options.add_argument("--disable-dev-shm-usage")  # Для Linux
-
-        service = ChromeService()  # Убедитесь, что chromedriver доступен в PATH
-        driver = webdriver.Chrome(service=service, options=options)
-        return driver
 
     def load_section_names(self):
         """Загрузка полных названий секций из JSON-файла."""
@@ -137,48 +130,18 @@ class PlayerokAutomation:
             logging.error(f"Ошибка при чтении JSON из 'game_names.json': {e}")
             sys.exit(1)
 
-    def login_to_playerok(self):
-        """Авторизация с загрузкой кук."""
-        url = "https://playerok.com"
-        self.driver.get(url)
-        self.load_cookies_from_file()
-
-    def load_cookies_from_file(self):
-        """Загрузка кук из файла и добавление их в браузер через Selenium."""
-        try:
-            with open('cookies_data.ckjson', 'r') as file:
-                cookies = json.load(file)
-        except FileNotFoundError:
-            logging.error("Файл с куками 'cookies_data.ckjson' не найден.")
-            return
-        except json.JSONDecodeError as e:
-            logging.error(f"Ошибка при чтении JSON из 'cookies_data.ckjson': {e}")
-            return
-
-        for cookie in cookies:
-            # Удаляем параметры, которые Selenium не принимает
-            for key in ['sameSite', 'storeId', 'hostOnly']:
-                if key in cookie:
-                    del cookie[key]
-            try:
-                self.driver.add_cookie(cookie)
-            except Exception as e:
-                logging.error(f"Ошибка при добавлении куки: {e}")
-
-        self.driver.refresh()
-
     def start_sell(self):
         """Выполнение действий на странице продажи."""
         url = "https://playerok.com/sell"
         try:
-            self.driver.get(url)
+            self.auth_manager.driver.get(url)
         except Exception as e:
             logging.error(f"Не удалось открыть страницу продажи: {e}")
             self.retry_entire_sell()
             return
 
         # Ожидание загрузки страницы
-        wait = WebDriverWait(self.driver, 35)
+        wait = WebDriverWait(self.auth_manager.driver, 35)
 
         # Выбор секции
         if not self.select_section(wait):
@@ -200,7 +163,7 @@ class PlayerokAutomation:
         """Проверка наличия сообщения 'Попробуйте позже' на странице."""
         try:
             # Используем точный XPath для обнаружения сообщения
-            retry_message = WebDriverWait(self.driver, 5).until(
+            retry_message = WebDriverWait(self.auth_manager.driver, 5).until(
                 EC.presence_of_element_located(
                     (By.XPATH, "//div[contains(text(), 'Попробуйте позже')]")
                 )
@@ -216,7 +179,8 @@ class PlayerokAutomation:
         attempt = 1
         while attempt <= self.MAX_RETRIES:
             delay = self.BASE_DELAY * (2 ** (attempt - 1))  # Экспоненциальная задержка
-            logging.info(f"Попытка {attempt} из {self.MAX_RETRIES}: Повторный запуск процесса продажи через {delay} секунд.")
+            logging.info(
+                f"Попытка {attempt} из {self.MAX_RETRIES}: Повторный запуск процесса продажи через {delay} секунд.")
             time.sleep(delay)
             try:
                 self.start_sell()
@@ -224,7 +188,8 @@ class PlayerokAutomation:
             except Exception as e:
                 logging.error(f"Попытка {attempt}: Ошибка при повторном запуске процесса продажи: {e}")
                 attempt += 1
-        logging.error(f"Достигнуто максимальное количество попыток ({self.MAX_RETRIES}) для карточки '{self.card['name']}'. Пропуск.")
+        logging.error(
+            f"Достигнуто максимальное количество попыток ({self.MAX_RETRIES}) для карточки '{self.card['name']}'. Пропуск.")
 
     def select_section(self, wait):
         """Выбор раздела на странице продажи на основе номера секции."""
@@ -260,9 +225,9 @@ class PlayerokAutomation:
         xpath = "//button[@type='submit']"
         attempt = 0
         while attempt < max_attempts:
-            but_submit = self.driver.find_element(By.XPATH, xpath)
+            but_submit = self.auth_manager.driver.find_element(By.XPATH, xpath)
             if but_submit.is_enabled() and not but_submit.get_attribute("disabled"):
-                self.driver.execute_script("arguments[0].click();", but_submit)
+                self.auth_manager.driver.execute_script("arguments[0].click();", but_submit)
                 logging.info("Кнопка 'Далее' нажата успешно.")
                 return
             else:
@@ -306,7 +271,7 @@ class PlayerokAutomation:
             EC.presence_of_element_located((By.XPATH, "//input[@name='title']"))
         )
         name = self.card["name"]
-        self.driver.execute_script("arguments[0].value = arguments[1];", name_input, name)
+        self.auth_manager.driver.execute_script("arguments[0].value = arguments[1];", name_input, name)
         name_input.send_keys(" ")
         logging.info(f"Поле названия заполнено значением '{name}'.")
 
@@ -318,7 +283,7 @@ class PlayerokAutomation:
         desc_input = wait.until(
             EC.presence_of_element_located((By.XPATH, "//textarea[@name='description']"))
         )
-        self.driver.execute_script("arguments[0].value = arguments[1];", desc_input, self.virt_description)
+        self.auth_manager.driver.execute_script("arguments[0].value = arguments[1];", desc_input, self.virt_description)
         desc_input.send_keys(" ")
         logging.info("Поле описания заполнено.")
 
@@ -327,8 +292,8 @@ class PlayerokAutomation:
     @retry_on_exception(max_retries=5, base_delay=20, backoff_factor=2)
     def fill_price_field(self, wait):
         """Заполнение поля цены."""
-        if self.url and self.driver.current_url != self.url:
-            self.driver.get(self.url)
+        if self.url and self.auth_manager.driver.current_url != self.url:
+            self.auth_manager.driver.get(self.url)
 
         price_input = wait.until(
             EC.presence_of_element_located((By.XPATH, "//input[@name='price']"))
@@ -346,7 +311,8 @@ class PlayerokAutomation:
         product_data_input = wait.until(
             EC.presence_of_element_located((By.XPATH, "//textarea[@name='comment']"))
         )
-        self.driver.execute_script("arguments[0].value = arguments[1];", product_data_input, self.product_data)
+        self.auth_manager.driver.execute_script("arguments[0].value = arguments[1];", product_data_input,
+                                                self.product_data)
         product_data_input.send_keys(" ")
         logging.info("Поле данных продукта заполнено.")
 
@@ -359,8 +325,8 @@ class PlayerokAutomation:
     def check_button(self):
         try:
             # Ищем кнопку с указанными атрибутами
-            button = self.driver.find_element(By.XPATH,
-                                         "//button[@type='button' and text()='Выставить бесплатно на 30 дней']")
+            button = self.auth_manager.driver.find_element(By.XPATH,
+                                                           "//button[@type='button' and text()='Выставить бесплатно на 30 дней']")
             return True  # Кнопка найдена
         except NoSuchElementException:
             return False  # Кнопка не найдена
@@ -380,7 +346,8 @@ class PlayerokAutomation:
     def exhibit_card(self, wait):
         """Выставление карточки."""
         exhibit_button = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@type='button' and text()='Выставить бесплатно на 30 дней']"))
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@type='button' and text()='Выставить бесплатно на 30 дней']"))
         )
         exhibit_button.click()
         logging.info("Кнопка 'Выставить бесплатно на 30 дней' нажата.")
@@ -388,10 +355,10 @@ class PlayerokAutomation:
 
     def navigate_edit_and_other_page(self):
         """Добавление /edit к текущему URL и переход на другую страницу."""
-        self.url = self.driver.current_url
+        self.url = self.auth_manager.driver.current_url
         time.sleep(2)
         try:
-            current_url = self.driver.current_url
+            current_url = self.auth_manager.driver.current_url
             if current_url.endswith('/status'):
                 edit_url = current_url.replace('/status', '/edit')
             else:
@@ -400,7 +367,7 @@ class PlayerokAutomation:
                 else:
                     edit_url = current_url + '/edit'
 
-            self.driver.get(edit_url)
+            self.auth_manager.driver.get(edit_url)
             logging.info(f"Переход на страницу: {edit_url}")
         except Exception as e:
             logging.error(f"Ошибка при навигации: {e}")
@@ -419,7 +386,6 @@ class PlayerokAutomation:
 
         self.click_submit_button(wait)
         time.sleep(5)
-
 
     @retry_on_exception(max_retries=5, base_delay=20, backoff_factor=2)
     def fill_common_fields(self, wait):
@@ -451,7 +417,7 @@ class PlayerokAutomation:
             specific_button.click()
 
             # Нажатие кнопки "Продать"
-            sell_button = self.driver.find_element(By.ID, "sell_button_id")
+            sell_button = self.auth_manager.driver.find_element(By.ID, "sell_button_id")
             sell_button.click()
 
             # Ожидание подтверждения
@@ -573,24 +539,22 @@ class PlayerokAutomation:
                 print("Получено сообщение 'попробуйте позже' в разделе 1.")
                 raise Exception("попробуйте позже")
 
-    def close_browser(self):
-        """Закрытие браузера"""
-        self.driver.quit()
-
 
 def run_bot_for_card(section_number, card, product_data, virt_description, delay=0):
     """Функция для запуска бота с возможной задержкой и обработкой ошибок"""
     if delay > 0:
         logging.info(f"Задержка перед запуском карточки '{card['name']}' на {delay:.2f} секунд.")
         time.sleep(delay)  # Задержка перед началом
-    bot = PlayerokAutomation(section_number, card, product_data, virt_description)
+
+    auth_manager = AuthManager()
+    bot = PlayerokAutomation(section_number, card, product_data, virt_description, auth_manager)
     try:
-        bot.login_to_playerok()
+        auth_manager.login()
         bot.start_sell()
     except Exception as e:
         logging.error(f"Общая ошибка при обработке карточки '{card['name']}': {e}")
     finally:
-        bot.close_browser()
+        auth_manager.close()
 
 
 def main():
@@ -656,7 +620,7 @@ def main():
         pool.close()
         pool.join()
 
-    print("Все процессы завершены.")
+    logging.info("Все процессы завершены.")
 
 
 if __name__ == "__main__":
